c2f79865298a4ecb52cce93ae30c8b37b572c453
EricPERONNIN <58256212+EricPERONNIN@users.noreply.github.com>
2020-05-14 09:50:54 +0200

    Update KicadToLTspice.bat

--- KiCadToLTspice/KicadToLTspice.bat
+++ KiCadToLTspice/KicadToLTspice.bat
@@ -1,5 +1,7 @@
 cd C:/Users/%username%/Documents/KiCadToLTspice/
-start pythonw3 KicadToLTspice.py %1
+start python KicadToLTspice.py %1
+rem start pythonw3 KicadToLTspice.py %1
+echo If you want to hide the cmd windows when you start the KiCadToLTspice script, comment by rem the start python line and uncomment the start pythonw3
 rem python3 KicadToLTspice.py %1
 rem C:\Programmes\LTC\LTspiceXVII\XVIIx64.exe -ascii %~dpn1LTspice.net
 rempause
